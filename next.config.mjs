7887cefa10d1066896329bc477d87919fa4ca018
Salil Lakra <btech10268.24@bitmesra.ac.in>
2024-11-06 10:48:44 +0530

    Remove unused SVG files and components; update image domains in configuration for coming soon page

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['assets.aceternity.com'],
-    },
+  images: {
+    domains: ["api.microlink.io"],
+  },
 };
 
 export default nextConfig;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,5 @@
 import React from "react";
-import Local_Hero from "../LocalComponents/Local_Hero";
-import Local_AuraText from "@/LocalComponents/Local_AuraText";
-import Local_TimeLineEffect from "@/LocalComponents/Local_TimeLineEffect";
 
 export default function FlipWordsDemo() {
-  return (
-    <div className="snap-y snap-mandatory overflow-y-scroll">
-      <Local_Hero className="snap-center snap-always" />
-      <div className="grid h-fit snap-center snap-always place-items-center bg-transparent">
-        <Local_AuraText />
-      </div>
-      <Local_TimeLineEffect />
-    </div>
-  );
+  return <div>coming soon</div>;
 }
